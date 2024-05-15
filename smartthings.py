3f857d5c9ec67da1f8f7f5aa72ccda58f8b39502
Philipp <124693539+phipsrick@users.noreply.github.com>
2024-05-15 17:50:10 +0200

    Update smartthings.py

--- smartthings.py
+++ smartthings.py
@@ -1,7 +1,21 @@
 import click
+from home_messages_db import HomeMessagesDB
+import gzip
+import pandas as pd
 
 @click.command()
 @click.option('-d', '--dburl', required=True, help='Database URL')
 @click.argument('files', nargs=-1, type=click.Path(exists=True))
-def main():
-    Pass
+def main(dburl, files):
+    db = HomeMessagesDB(dburl)
+    
+    for file in files:
+        with gzip.open(file, 'rt') as f:
+            df = pd.read_csv(f, delimiter='\t')
+            df.fillna('', inplace=True)  
+            data = df.to_dict(orient='records')
+            db.add_smartthings(data)  
+            print(f"Inserted data from {file} into the database.")
+
+if __name__ == '__main__':
+    main()
