c4b7bb614c6891b87a7b4461f6740c655fe447d6
Philipp <124693539+phipsrick@users.noreply.github.com>
2024-05-15 11:02:22 +0200

    Update home_messages_db.py

--- home_messages_db.py
+++ home_messages_db.py
@@ -12,21 +12,23 @@ class SmartThings(Base):
     epoch = Column(Integer)
     capability = Column(String)
     attribute = Column(String)
-    value = Column(String)
+    value = Column(Float)
     unit = Column(String)
 
 class P1e(Base):
     __tablename__ = 'p1e'
     id = Column(Integer, primary_key=True)
     time = Column(Integer)
-    energy_usage_t1 = Column(Float)
-    energy_usage_t2 = Column(Float)
+    electricity_imported_t1 = Column(Float)
+    electricity_imported_t2= Column(Float)
+    electricity_exported_t1 = Column(Float)
+    electricity_exported_t2 = Column(Float)  
 
 class P1g(Base):
     __tablename__ = 'p1g'
     id = Column(Integer, primary_key=True)
     time = Column(Integer)
-    gas_usage = Column(Float)
+    total_gas_used = Column(Float)
 
 class HomeMessagesDB:
     pass
