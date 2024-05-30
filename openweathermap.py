9422f08cee14e63f9404d322bf2a3a38ffe2210d
Philipp <124693539+phipsrick@users.noreply.github.com>
2024-05-30 15:11:12 +0200

    Update openweathermap.py
    increase coherence with smartthings.py

--- openweathermap.py
+++ openweathermap.py
@@ -6,6 +6,7 @@ import requests
 @click.command()
 @click.option('-d', '--dburl', required=True, help='Database URL')
 def main(dburl):
+    db = HomeMessagesDB(dburl)
     url = "https://archive-api.open-meteo.com/v1/era5?latitude=52.19&longitude=4.44&timeformat=unixtime&start_date=2022-01-01&end_date=2024-04-30&hourly=temperature_2m,relativehumidity_2m,rain,snowfall,windspeed_10m,winddirection_10m,soil_temperature_0_to_7cm"
     download = requests.get(url)
     if download.status_code == 200:
@@ -13,8 +14,6 @@ def main(dburl):
         df = pd.DataFrame(data['hourly'])
         df.fillna('', inplace=True) 
         data = df.to_dict(orient='records')
-
-        db = HomeMessagesDB(dburl)
         db.add_weather_data(data)
         print(f"Inserted data from Open Meteo into the database.")
     else:
