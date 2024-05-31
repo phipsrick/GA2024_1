156ae1e00d4c7fb99a7cbed32accfb255410059f
Philipp <124693539+phipsrick@users.noreply.github.com>
2024-05-31 12:39:38 +0200

    Update p1e.py
    remove else if statement

--- p1e.py
+++ p1e.py
@@ -12,23 +12,20 @@ def main(dburl, files):
     amsterdam_tz = pytz.timezone('Europe/Amsterdam')
     
     for file in files:
-        if file.startswith('P1e-'):
-            with gzip.open(file, 'rt') as f:
-                df = pd.read_csv(f)
-                # Select only the relevant columns
-                df = df[['time', 'Electricity imported T1', 'Electricity imported T2', 'Electricity exported T1', 'Electricity exported T2']]
-                df.columns = ['time', 't1_imported', 't2_imported', 't1_exported', 't2_exported']
+        with gzip.open(file, 'rt') as f:
+            df = pd.read_csv(f)
+            # Select only the relevant columns
+            df = df[['time', 'Electricity imported T1', 'Electricity imported T2', 'Electricity exported T1', 'Electricity exported T2']]
+            df.columns = ['time', 't1_imported', 't2_imported', 't1_exported', 't2_exported']
                 
-                # Convert time to Europe/Amsterdam timezone
-                df['time'] = pd.to_datetime(df['time']).dt.tz_localize('UTC').dt.tz_convert(amsterdam_tz)
+            # Convert time to Europe/Amsterdam timezone
+            df['time'] = pd.to_datetime(df['time']).dt.tz_localize('UTC').dt.tz_convert(amsterdam_tz)
                 
-                df.fillna('', inplace=True)
-                data = df.to_dict(orient='records')
+            df.fillna('', inplace=True)
+            data = df.to_dict(orient='records')
                 
-                db.add_p1e(data)
-                print(f"Inserted data from {file} into the database.")
-        else:
-            print(f"Skipped file {file} as it does not start with 'P1e-'")
+            db.add_p1e(data)
+            print(f"Inserted data from {file} into the database.")
 
 if __name__ == '__main__':
     main()
